57cf1441d6c09393f3597eb2a9acaa81325dbf16
Guille <guille.terren@gmail.com>
2025-03-03 14:35:54 -0800

    reliability metrics descrition and humand friendly metrics display

--- .ipynb_checkpoints/processing_utils-checkpoint.py
+++ .ipynb_checkpoints/processing_utils-checkpoint.py
@@ -34,7 +34,6 @@ def _resource_adequacy(df_):
 
     df_p_ = df_.copy()
     
-    df_p_['demand_hr']       = (df_p_['static_load_mw'] > 0.)*1.
     df_p_['loss_of_load_hr'] = (df_p_['unserved_energy_mw'] > 0.)*1.
 
     df_p_['LOLF'] = np.diff(df_p_['loss_of_load_hr'].to_numpy(), prepend = 0)
@@ -44,13 +43,21 @@ def _resource_adequacy(df_):
                             'scenario']).agg({'overgeneration_mw': 'sum', 
                                               'unserved_energy_mw': 'sum', 
                                               'static_load_mw': 'sum', 
-                                              'demand_hr': 'sum',
                                               'loss_of_load_hr': 'sum', 
                                               'LOLF': 'sum'}).reset_index(drop = False)
-
-    df_pp_['EUE_%']   = 100.*df_pp_['unserved_energy_mw']/df_pp_['static_load_mw']
-    df_pp_['LOLP']    = df_pp_['loss_of_load_hr']/df_pp_['demand_hr']
-    df_pp_['LOLD_hr'] = df_pp_['loss_of_load_hr']/df_pp_['LOLF']
+    N = df_p_['unserved_energy_mw'].shape[0]
+    df_pp_['EUE']  = 100.*df_pp_['unserved_energy_mw']/df_pp_['static_load_mw']
+    df_pp_['LOLP'] = df_pp_['loss_of_load_hr']/N
+    df_pp_['LOLD'] = df_pp_['loss_of_load_hr']/df_pp_['LOLF']
+    
+    df_pp_ = df_pp_.rename(columns = {'overgeneration_mw':  'Overgeneration (MWh)',
+                                      'unserved_energy_mw': 'Unserved Energy (MWh)',
+                                      'static_load_mw':     'Total Demand (MWh)',
+                                      'loss_of_load_hr':    'Loss-Of-Load Events (hr)',
+                                      'EUE':                'Expected Unserved Energy (%)',
+                                      'LOLP':               'Loss-Of-Load Probability',
+                                      'LOLF':               'Loss-Of-Load Frequency (per period)',
+                                      'LOLD':               'Loss-Of-Load Duration (hr)'})
     
     return df_pp_.set_index('scenario').T
     
